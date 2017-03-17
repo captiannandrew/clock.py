83f7c92d0375bd1b4967ac7ad998ba416f683e74
captiannandrew <andrewischau@gmail.com>
2017-03-16 17:46:56 -0700

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -9,10 +9,14 @@ alarm = raw_input(">")
 Time = time.strftime("%H:%M")
 
 while Time != alarm:
+    #sec_time will show the amount of time that is passing in seconds.
     sec_time = time.strftime("%H:%M:%S")
     
     print "The time is:" + sec_time 
+    
+    #updates time
     Time = time.strftime("%H:%M")
+    #waits for a second so there is no extra time printed
     time.sleep(1)
     
     if Time == alarm:
